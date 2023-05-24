44e0340d4b6cb1203f088b5617701cd2314930fd
felipe-programador-18 <machadofelipe2016@outlook.com>
2023-05-24 13:43:04 -0300

    starting a new test project

---
+++ src/app.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hi I come back put my hands on the code, to fixed and get evolv my knowlegde each day more';
+  }
+}
